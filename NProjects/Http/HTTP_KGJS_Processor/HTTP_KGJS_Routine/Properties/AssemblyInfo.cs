bad81d49f335b627d85f12e5ea6fb26c386dee82
KalpitaL <kalpita@lighthouse.esolutions.com>
2020-05-07 18:32:57 +0530

    KGJS_Http

---
+++ NProjects/Http/HTTP_KGJS_Processor/HTTP_KGJS_Routine/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("HTTP_KGJS_Routine")]
+[assembly: AssemblyDescription("KGJS HTML Routine")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Lighthouse eSolutions Pvt Lte")]
+[assembly: AssemblyProduct("HTTP_KGJS_Routine")]
+[assembly: AssemblyCopyright("Copyright © LeS 2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("5b55b8c6-d322-4ffe-aeef-c6b138c5ef6e")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("20.05.05.0")]
+[assembly: AssemblyFileVersion("20.05.05.0")]
